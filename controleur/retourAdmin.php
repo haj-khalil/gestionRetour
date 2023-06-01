d545a7459b27d9e58e729040d2c887b53c203ae3
dialloxavi06 <107733204+dialloxavi06@users.noreply.github.com>
2023-06-01 13:34:58 +0200

    2eme commit du 01juin

--- controleur/retourAdmin.php
+++ controleur/retourAdmin.php
@@ -32,10 +32,10 @@ if ((time() - $_SESSION['last_login']) > 900 && $_SESSION['login'] != "root") {
     } else $isAdmin = false;
 
     $id                         = isset($_GET['id']) ? $_GET['id'] : null;
-     $input_id_statut            = isset($_GET['select_id_statut']) ? $_GET['select_id_statut'] : null;
-     $date_remboursement         = isset($_GET['date_remboursement']) ? $_GET['date_remboursement'] : null;
-     $updateStatut               = isset($_GET['updateStatut']) ? $_GET['updateStatut'] : null;
-     $id_retour_modif_statut     = isset($_GET['id_retour_modif_statut']) ? $_GET['id_retour_modif_statut'] : 2;
+    $input_id_statut            = isset($_GET['select_id_statut']) ? $_GET['select_id_statut'] : null;
+    $date_remboursement         = isset($_GET['date_remboursement']) ? $_GET['date_remboursement'] : null;
+    $updateStatut               = isset($_GET['updateStatut']) ? $_GET['updateStatut'] : null;
+    $id_retour_modif_statut     = isset($_GET['id_retour_modif_statut']) ? $_GET['id_retour_modif_statut'] : 2;
     $adminRechercheClientRetour = isset($_GET['adminRechercheClientRetour']) ? $_GET['adminRechercheClientRetour'] : null;
     $EmailClient                = isset($_GET['EmailClient']) ? $_GET['EmailClient'] : null;
     $op                         = isset($_GET['op']) ? $_GET['op'] : null;
@@ -76,11 +76,11 @@ if ((time() - $_SESSION['last_login']) > 900 && $_SESSION['login'] != "root") {
     if ($lesArticles != []) {
         $lesRows[] = "
 				<tr class='article'>
-				<th colspan='2'>Article</th>
-				<th colspan='2'>Numéro</th>
-				<th colspan='2'> Montant</th>
-				<th colspan='2'>Quantité</th>
-				<th colspan='3'>Motif</th>
+				<th colspan='2'>nom_article </th>
+				<th colspan='2'>id_article</th>
+				<th colspan='2'> montant_piece</th>
+				<th colspan='2'>quantite</th>
+				<th colspan='3'>motif</th>
 				
 
 				<th><button type='button'><img id='img_x'  onclick='casherTableArticle()' src='../vue/style/x.jpg'></button>
@@ -149,6 +149,7 @@ if ((time() - $_SESSION['last_login']) > 900 && $_SESSION['login'] != "root") {
             '"><img src="../vue/style/ajout.png"></a></td>';
 
         $ch .= '<td><input type="button" onclick="getIdRetour(this.value)" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalCenter"
+	    " value=' . urlencode($unRetour['id_retour']) . '></input></td>';
 
 
         $lignes[] = "<tr>$ch</tr>";
@@ -190,7 +191,6 @@ if ((time() - $_SESSION['last_login']) > 900 && $_SESSION['login'] != "root") {
     }
     unset($lesRetours);
 
-    require_once('../vue/retourView.php');
     require_once('../vue/retourView.php');
 } else {
     echo "<h2 style=' text-align: center;'>Désolé, il y a une erreur : vous ne pouvez pas accéder à cette page.</h2>";
