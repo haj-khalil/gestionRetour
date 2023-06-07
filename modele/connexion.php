a25808570cb769cb9a3d3493cf41720238e8ae2e
HajKhalil Abdulhamid <abdulhamid.haj-khalil1@etu.univ-lorraine.fr~>
2023-06-07 05:30:06 +0200

    test la connexion

--- modele/connexion.php
+++ modele/connexion.php
@@ -7,11 +7,16 @@ class Connexion {
 
 	function __construct() {
 	
-		$db_config['SGBD']		= 'mysql';
-		$db_config['HOST']		= '127.0.0.1:3307';
-		$db_config['DB_NAME']	= 'suivi_des_retours';
+        $db_config['SGBD']		= 'mysql';
+		$db_config['HOST']		= '127.0.0.2:3307';
+		$db_config['DB_NAME']	= 'sae4';
 		$db_config['USER']		= 'root';
-		$db_config['PASSWORD']	= 'root';
+		$db_config['PASSWORD']	= '';
+        /*  $db_config['SGBD']		= 'mysql';
+		$db_config['HOST']		= 'devbdd.iutmetz.univ-lorraine.fr';
+		$db_config['DB_NAME']	= 'hajkhali1u_sae4';
+		$db_config['USER']		= 'hajkhali1u_appli';
+		$db_config['PASSWORD']	= '32219666'; */
 		try
 		{
 			$this->db = new PDO($db_config['SGBD'] .':host='. $db_config['HOST'] .';dbname='. $db_config['DB_NAME'],
