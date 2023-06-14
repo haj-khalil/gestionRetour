66511f55afa0899b31219971ac6fa4a674c99f14
dialloxavi06 <107733204+dialloxavi06@users.noreply.github.com>
2023-06-14 10:55:18 +0200

    commit du 14juin

--- vue/footer.php
+++ vue/footer.php
@@ -1,3 +1,11 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Retour des Articles</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.1.3/css/bootstrap.min.css">
+</head>
 <body class="d-flex flex-column min-vh-100">
     <main class="flex-grow-1">
         <!-- Contenu de la page ici -->
@@ -8,31 +16,31 @@
             <div class="row">
                 <div class="col-lg-6">
                     <h5>A propos</h5>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    <p>Nous sommes une plateforme en ligne dédiée au retour des articles. Notre objectif est de rendre le processus de retour aussi simple et pratique que possible pour nos clients. Vous pouvez compter sur notre équipe dédiée pour vous accompagner à chaque étape du processus de retour et vous assurer une expérience sans tracas.</p>
                 </div>
                 <div class="col-lg-3">
                     <h5>Liens utiles</h5>
                     <ul class="list-unstyled">
                         <li><a href="#">Accueil</a></li>
-                        <!-- <span class="text-muted">© 2023 Tous droits réservés.</span> -->
+                        <li><a href="#">Articles</a></li>
+                        <li><a href="#">Politique de retour</a></li>
                     </ul>
                 </div>
                 <div class="col-lg-3">
                     <h5>Contact</h5>
                     <ul class="list-unstyled">
-                        <li><i class="bi bi-envelope-fill"><a href="mailto:diallo@example.com">contactez nous</a></i> </li>
+                        <li><i class="bi bi-envelope-fill"></i> <a href="mailto:diallo@example.com">Contactez-nous</a></li>
                     </ul>
                 </div>
             </div>
         </div>
-       <!--  <div class="container-fluid bg-secondary py-3">
+        <div class="container-fluid bg-secondary py-3">
             <div class="container text-center">
                 <span class="text-muted">© 2023 Tous droits réservés.</span>
             </div>
-        </div> -->
-        
-        
+        </div>
     </footer>
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.1.3/js/bootstrap.bundle.min.js"></script>
 </body>
+</html>
